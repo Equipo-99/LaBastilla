a4ebc007c156eeb0f5b5676436bc4770861af77d
Daniel Vasquez <danielvf2396@gmail.com>
2022-08-27 17:10:00 -0500

    Marco de trabajo en Spring Boot

---
+++ src/main/java/edu/udea/LaBastilla/LaBastillaApplication.java
@@ -0,0 +1,13 @@
+package edu.udea.LaBastilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaBastillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaBastillaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/udea/LaBastilla/LaBastillaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.udea.LaBastilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaBastillaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
